4c560817ccf5e00939dda8c36ff01d6ecb8bc4c2
Flo <florian.unternaehrer@simplificator.com>
2018-01-22 13:22:59 +0100

    add basic circle, with some rings and some separators

--- state/reducer.js
+++ state/reducer.js
@@ -1,19 +1,44 @@
+import { range } from "ramda"
+
 
 const initialState = {
-  tasks: []
+  fromDate: new Date().setFullYear(new Date().getFullYear() - 1),
+  toDate: new Date(),
+  seperators: [],
+  rings: ["#bed4e6", "#ccdfee", "#dcecf9", "#f0f8ff"],
+  height: 600
+}
+
+
+const dayDiff = (a, b) => {
+  return Math.round((b - a) / (1000 * 60 * 60 * 24))
+}
+
+
+const generateSeparators = (state) => {
+  const { fromDate, toDate } = state
+  const days = dayDiff(fromDate, toDate)
+
+  const separators = range(0, days).reduce((acc, x) => {
+    const date = new Date(fromDate + x * (1000*60*60*24))
+    if (date.getDate() === 1) {
+      return [...acc, [360 / days * x, date.getMonth()]]
+    } else {
+      return acc
+    }
+  }, [])
+
+  return {
+    ...state,
+    separators
+  }
 }
 
 
 export default (state = initialState, action) => {
   switch (action.type) {
 
-    // case "GAME_STATE_CHANGE":
-    //   return updateGameState(state, action.state)
-    //
-    // case "SWITCH_PLAYER":
-    //   return switchPlayer(state, action.nextPlayer)
-
     default:
-      return state
+      return generateSeparators(state)
   }
 }
--- view/main.js
+++ view/main.js
@@ -5,7 +5,6 @@ import { connect } from "react-redux"
 require("./style.scss")
 
 
-
 // React Redux Bindings
 
 const mapStateToProps = (state) => state
@@ -19,8 +18,45 @@ const mapDispatchToProps = (dispatch) => {
 
 // Main Component
 
-const main = () =>
-  <h1>Overview</h1>
+const main = ({ height, rings, separators }) => {
+  const midPoint = height / 2
+  const width = midPoint / (rings.length + 1)
+  const ringsAndCenter = [...rings, "#fff"]
+
+  return (
+    <div>
+      <svg height={height} width={height}>
+        <g>
+          { ringsAndCenter.map((color, i) => {
+              const invserseI = rings.length - i + 1
+              return (
+                <circle
+                  key={color}
+                  cx={midPoint}
+                  cy={midPoint}
+                  r={invserseI * width}
+                  fill={color}
+                  stroke="#f8f9f9a1"
+                  />
+              )
+            })
+          }
+        </g>
+        <g>
+          <line x1={midPoint} y1={midPoint - width} x2={midPoint} y2={0} stroke="#00000040" />
+          { separators.map(([angle, month]) => {
+            const x2 = midPoint + midPoint * Math.cos(Math.PI * angle / 180.0)
+            const y2 = midPoint + midPoint * Math.sin(Math.PI * angle / 180.0)
+
+            return (
+              <line key={angle} x1={midPoint} y1={midPoint} x2={x2} y2={y2} stroke="#fff" />
+            )
+          }) }
+        </g>
+      </svg>
+    </div>
+  )
+}
 
 
 
